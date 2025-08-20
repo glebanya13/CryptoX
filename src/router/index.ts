79d24c896192a611b4b8f7d4c1f7a0c1f39d7e19
Глеб <gleb@MacBook-Air-Gleb.local>
2025-08-20 22:48:40 +0300

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,6 @@ import ProfilePage from "../pages/profile/ProfilePage.vue";
 import ConfirmPage from "../pages/confirm/ConfirmPage.vue";
 import SignUpPage from "../pages/auth/SignUpPage.vue";
 import SignInPage from "../pages/auth/SignInPage.vue";
-import ContactUsPage from "../pages/contact/ContactUsPage.vue";
-import AboutUsPage from "../pages/about/AboutUsPage.vue";
 import ReviewsPage from "../pages/reviews/ReviewsPage.vue";
 import NewsPage from "../pages/news/NewsPage.vue";
 import NewsDetailPage from "../pages/news/NewsDetailPage.vue";
@@ -17,8 +15,6 @@ const routes = [
   { path: "/user-data", component: ConfirmPage },
   { path: "/sign-up", component: SignUpPage },
   { path: "/sign-in", component: SignInPage },
-  { path: "/contact-us", component: ContactUsPage },
-  { path: "/about-us", component: AboutUsPage },
   { path: "/reviews", component: ReviewsPage },
   { path: "/news", component: NewsPage },
   { path: "/news/:id", component: NewsDetailPage },
